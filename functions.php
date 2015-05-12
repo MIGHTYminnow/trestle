8a6adb74003b1f98c3839e45e71cc21b19005cfc
MickeyKay <mickeyskay@gmail.com>
2015-05-11 17:13:51 -0700

    Fix whitespace.

--- functions.php
+++ functions.php
@@ -33,6 +33,7 @@ require_once dirname( __FILE__ ) . '/includes/classes/class-tgm-plugin-activatio
 // Better Font Awesome Library.
 require_once dirname( __FILE__ ) . '/lib/better-font-awesome-library/better-font-awesome-library.php';
 
+
 add_action( 'genesis_setup', 'trestle_theme_setup', 15 );
 /**
  * Initialize Trestle.
