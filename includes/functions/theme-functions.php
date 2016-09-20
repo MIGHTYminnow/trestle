d0d2a0aef811c236b501a0749fa3ab798def4d5f
jgonzo <jgonzo127@gmail.com>
2016-09-20 10:22:42 -0700

    Add options for sub menu hover colors and footer colors

--- includes/functions/theme-functions.php
+++ includes/functions/theme-functions.php
@@ -137,55 +137,63 @@ add_action( 'wp_head', 'trestle_output_styles');
 function trestle_output_styles() {
 
 	$styles = array();
-	$site_title_color             = trestle_get_option( 'site_title_color' );
-	$site_description_color       = trestle_get_option( 'site_description_color' );
-	$site_bg_color                = trestle_get_option( 'site_bg_color' );
-	$body_text_color              = trestle_get_option( 'body_text_color' );
-	$body_link_color              = trestle_get_option( 'body_link_color' );
-	$body_link_hover_color        = trestle_get_option( 'body_link_hover_color' );
-	$entry_title_link_color       = trestle_get_option( 'entry_title_link_color' );
-	$entry_title_link_hover_color = trestle_get_option( 'entry_title_link_hover_color' );
-	$header_bg_color              = trestle_get_option( 'header_bg_color' );
-	$menu_bg_color                = trestle_get_option( 'menu_bg_color' );
-	$menu_text_color              = trestle_get_option( 'menu_text_color' );
-	$sub_menu_bg_color            = trestle_get_option( 'sub_menu_bg_color' );
-	$sub_menu_text_color          = trestle_get_option( 'sub_menu_text_color' );
-	$sidebar_text_color           = trestle_get_option( 'sidebar_text_color' );
-	$widget_title_color           = trestle_get_option( 'widget_title_color' );
-	$sidebar_link_color           = trestle_get_option( 'sidebar_link_color' );
-	$sidebar_link_hover_color     = trestle_get_option( 'sidebar_link_hover_color' );
-	$footer_bg_color              = trestle_get_option( 'footer_bg_color' );
-	$footer_link_color            = trestle_get_option( 'footer_link_color' );
-	$footer_link_hover_color      = trestle_get_option( 'footer_link_hover_color' );
-	$h1_text_color                = trestle_get_option( 'h1_text_color' );
-	$h2_text_color                = trestle_get_option( 'h2_text_color' );
-	$h3_text_color                = trestle_get_option( 'h3_text_color' );
-	$h4_text_color                = trestle_get_option( 'h4_text_color' );
-	$h5_text_color                = trestle_get_option( 'h5_text_color' );
-	$h6_text_color                = trestle_get_option( 'h6_text_color' );
-	$site_title_font_size         = trestle_get_option( 'site_title_font_size' );
-	$site_description_font_size   = trestle_get_option( 'site_description_font_size' );
-	$body_font_family             = trestle_get_option( 'body_font_family' );
-	$h1_font_size                 = trestle_get_option( 'h1_font_size' );
-	$h2_font_size                 = trestle_get_option( 'h2_font_size' );
-	$h3_font_size                 = trestle_get_option( 'h3_font_size' );
-	$h4_font_size                 = trestle_get_option( 'h4_font_size' );
-	$h5_font_size                 = trestle_get_option( 'h5_font_size' );
-	$h6_font_size                 = trestle_get_option( 'h6_font_size' );
-	$h1_text_decoration           = trestle_get_option( 'h1_text_decoration' );
-	$h2_text_decoration           = trestle_get_option( 'h2_text_decoration' );
-	$h3_text_decoration           = trestle_get_option( 'h3_text_decoration' );
-	$h4_text_decoration           = trestle_get_option( 'h4_text_decoration' );
-	$h5_text_decoration           = trestle_get_option( 'h5_text_decoration' );
-	$h6_text_decoration           = trestle_get_option( 'h6_text_decoration' );
-	$h1_text_style                = trestle_get_option( 'h1_text_style' );
-	$h2_text_style                = trestle_get_option( 'h2_text_style' );
-	$h3_text_style                = trestle_get_option( 'h3_text_style' );
-	$h4_text_style                = trestle_get_option( 'h4_text_style' );
-	$h5_text_style                = trestle_get_option( 'h5_text_style' );
-	$h6_text_style                = trestle_get_option( 'h6_text_style' );
-	$layout                       = trestle_get_option( 'layout' );
-	$google_font_code             = trestle_get_option( 'google_font_code' );
+	$site_title_color                = trestle_get_option( 'site_title_color' );
+	$site_description_color          = trestle_get_option( 'site_description_color' );
+	$site_bg_color                   = trestle_get_option( 'site_bg_color' );
+	$body_text_color                 = trestle_get_option( 'body_text_color' );
+	$body_link_color                 = trestle_get_option( 'body_link_color' );
+	$body_link_hover_color           = trestle_get_option( 'body_link_hover_color' );
+	$entry_title_link_color          = trestle_get_option( 'entry_title_link_color' );
+	$entry_title_link_hover_color    = trestle_get_option( 'entry_title_link_hover_color' );
+	$header_bg_color                 = trestle_get_option( 'header_bg_color' );
+	$menu_bg_color                   = trestle_get_option( 'menu_bg_color' );
+	$menu_text_color                 = trestle_get_option( 'menu_text_color' );
+	$menu_text_hover_color           = trestle_get_option( 'menu_text_hover_color' );
+	$current_menu_item_color         = trestle_get_option( 'current_menu_item_color' );
+	$sub_menu_bg_color               = trestle_get_option( 'sub_menu_bg_color' );
+	$sub_menu_text_color             = trestle_get_option( 'sub_menu_text_color' );
+	$sub_menu_text_hover_color       = trestle_get_option( 'sub_menu_text_hover_color' );
+	$sidebar_text_color              = trestle_get_option( 'sidebar_text_color' );
+	$widget_title_color              = trestle_get_option( 'widget_title_color' );
+	$sidebar_link_color              = trestle_get_option( 'sidebar_link_color' );
+	$sidebar_link_hover_color        = trestle_get_option( 'sidebar_link_hover_color' );
+	$footer_bg_color                 = trestle_get_option( 'footer_bg_color' );
+	$footer_text_color               = trestle_get_option( 'footer_text_color' );
+	$footer_link_color               = trestle_get_option( 'footer_link_color' );
+	$footer_link_hover_color         = trestle_get_option( 'footer_link_hover_color' );
+	$footer_widgets_bg_color         = trestle_get_option( 'footer_widgets_bg_color' );
+	$footer_widgets_text_color       = trestle_get_option( 'footer_widgets_text_color' );
+	$footer_widgets_link_color       = trestle_get_option( 'footer_widgets_link_color' );
+	$footer_widgets_link_hover_color = trestle_get_option( 'footer_widgets_link_hover_color' );
+	$h1_text_color                   = trestle_get_option( 'h1_text_color' );
+	$h2_text_color                   = trestle_get_option( 'h2_text_color' );
+	$h3_text_color                   = trestle_get_option( 'h3_text_color' );
+	$h4_text_color                   = trestle_get_option( 'h4_text_color' );
+	$h5_text_color                   = trestle_get_option( 'h5_text_color' );
+	$h6_text_color                   = trestle_get_option( 'h6_text_color' );
+	$site_title_font_size            = trestle_get_option( 'site_title_font_size' );
+	$site_description_font_size      = trestle_get_option( 'site_description_font_size' );
+	$body_font_family                = trestle_get_option( 'body_font_family' );
+	$h1_font_size                    = trestle_get_option( 'h1_font_size' );
+	$h2_font_size                    = trestle_get_option( 'h2_font_size' );
+	$h3_font_size                    = trestle_get_option( 'h3_font_size' );
+	$h4_font_size                    = trestle_get_option( 'h4_font_size' );
+	$h5_font_size                    = trestle_get_option( 'h5_font_size' );
+	$h6_font_size                    = trestle_get_option( 'h6_font_size' );
+	$h1_text_decoration              = trestle_get_option( 'h1_text_decoration' );
+	$h2_text_decoration              = trestle_get_option( 'h2_text_decoration' );
+	$h3_text_decoration              = trestle_get_option( 'h3_text_decoration' );
+	$h4_text_decoration              = trestle_get_option( 'h4_text_decoration' );
+	$h5_text_decoration              = trestle_get_option( 'h5_text_decoration' );
+	$h6_text_decoration              = trestle_get_option( 'h6_text_decoration' );
+	$h1_text_style                   = trestle_get_option( 'h1_text_style' );
+	$h2_text_style                   = trestle_get_option( 'h2_text_style' );
+	$h3_text_style                   = trestle_get_option( 'h3_text_style' );
+	$h4_text_style                   = trestle_get_option( 'h4_text_style' );
+	$h5_text_style                   = trestle_get_option( 'h5_text_style' );
+	$h6_text_style                   = trestle_get_option( 'h6_text_style' );
+	$layout                          = trestle_get_option( 'layout' );
+	$google_font_code                = trestle_get_option( 'google_font_code' );
 
 	if ( ! empty( $site_title_color ) ) {
 		$styles[] = '.site-title a, .site-title a:hover { color: ' . $site_title_color . '; }';
@@ -232,13 +240,26 @@ function trestle_output_styles() {
 		$styles[] = '.nav-primary a, .nav-secondary a { color: ' . $menu_text_color . '; }';
 	}
 
+	if ( ! empty( $menu_text_hover_color ) ) {
+		$styles[] = '.nav-primary a:hover, .nav-secondary a:hover { color: ' . $menu_text_hover_color . '; }';
+	}
+
+	if ( ! empty( $current_menu_item_color ) ) {
+		$styles[] = '.nav-primary ul li.current-menu-item > a, .nav-primary ul li.current-menu-parent > a { color: ' . $current_menu_item_color . '; }';
+	}
+
 	if ( ! empty( $sub_menu_bg_color ) ) {
 		$styles[] = '.nav-primary .sub-menu { background-color: ' . $sub_menu_bg_color . '; }';
 	}
 
 	if ( ! empty( $sub_menu_text_color ) ) {
-		$styles[] = '.nav-primary .sub-menu a { color: ' . $sub_menu_text_color . '; border-color: ' . $sub_menu_text_color . '; }';
+		$styles[] = '.nav-primary .sub-menu a { color: ' . $sub_menu_text_color . '; }';
 		$styles[] = '.nav-primary .sub-menu-toggle .sub-menu-toggle-span { background-color: ' . $sub_menu_text_color . '; }';
+	}
+
+	if ( ! empty( $sub_menu_text_hover_color ) ) {
+		$styles[] = '.nav-primary .sub-menu a:hover { color: ' . $sub_menu_text_hover_color . '; }';
+		$styles[] = '.nav-primary .sub-menu-toggle .sub-menu-toggle-span { background-color: ' . $sub_menu_text_hover_color . '; }';
 	}
 
 	if ( ! empty( $sidebar_text_color ) ) {
@@ -262,7 +283,7 @@ function trestle_output_styles() {
 	}
 
 	if ( ! empty( $footer_text_color ) ) {
-		$styles[] = '.site-footer { color: ' . $footer_text_color . '; }';
+		$styles[] = '.site-footer p { color: ' . $footer_text_color . '; }';
 	}
 
 	if ( ! empty( $footer_link_color ) ) {
@@ -271,6 +292,22 @@ function trestle_output_styles() {
 
 	if ( ! empty( $footer_link_hover_color ) ) {
 		$styles[] = '.site-footer a:hover { color: ' . $footer_link_hover_color . '; }';
+	}
+
+	if ( ! empty( $footer_widgets_bg_color ) ) {
+		$styles[] = '.footer-widgets { background-color: ' . $footer_widgets_bg_color . '; }';
+	}
+
+	if ( ! empty( $footer_widgets_text_color ) ) {
+		$styles[] = '.footer-widgets p { color: ' . $footer_widgets_text_color . '; }';
+	}
+
+	if ( ! empty( $footer_widgets_link_color ) ) {
+		$styles[] = '.footer-widgets a { color: ' . $footer_widgets_link_color . '; }';
+	}
+
+	if ( ! empty( $footer_widgets_link_hover_color ) ) {
+		$styles[] = '.footer-widgets a:hover { color: ' . $footer_widgets_link_hover_color . '; }';
 	}
 
 	if ( ! empty( $h1_text_color ) ) {
