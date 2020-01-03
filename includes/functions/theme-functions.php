a875eec51b9131c4e926c12203b33b7a43976291
Aaron Barraza <aaron@mightyminnow.com>
2020-01-03 06:55:40 -0700

    TRESTLE 2: Loads theme scripts and styles.

--- includes/functions/theme-functions.php
+++ includes/functions/theme-functions.php
@@ -50,3 +50,51 @@ function trestle_remove_genesis_css_enqueue() {
 	remove_action( 'genesis_meta', 'genesis_load_stylesheet' );
 
 }
+
+/*===========================================
+ * Header
+===========================================*/
+
+add_action( 'wp_enqueue_scripts', 'trestle_header_actions', 15 );
+/**
+ * Loads theme scripts and styles.
+ *
+ * @since  1.0.0
+ */
+function trestle_header_actions() {
+
+	// Our main stylesheet.
+	wp_enqueue_style( 'trestle', get_stylesheet_uri(), array(), TRESTLE_THEME_VERSION );
+
+	// Google fonts.
+	wp_enqueue_style( 'theme-google-fonts', '//fonts.googleapis.com/css?family=Lato:300,400,700' );
+
+	// Theme jQuery.
+	wp_enqueue_script( 'theme-jquery', get_stylesheet_directory_uri() . '/includes/js/theme-jquery.js', array( 'jquery' ), TRESTLE_THEME_VERSION, true );
+
+	// Prepare and include some necessary variables.
+	$mobile_nav_text = apply_filters( 'trestle_mobile_nav_text', __( 'Navigation', 'trestle' ) );
+	wp_localize_script(
+		'theme-jquery',
+		'trestle_vars',
+		array(
+			'mobile_nav_text' => esc_attr( $mobile_nav_text ),
+		)
+	);
+
+	// Get WP uploads directory.
+	$upload_dir = wp_upload_dir();
+	$upload_path = $upload_dir['basedir'];
+	$upload_url = $upload_dir['baseurl'];
+
+	// Custom CSS (if it exists).
+	$custom_css_file = '/trestle/custom.css';
+	if ( is_readable( $upload_path . $custom_css_file ) )
+		wp_enqueue_style( 'trestle-custom-css', $upload_url . $custom_css_file );
+
+	// Custom jQuery (if it exists).
+	$custom_js_file = '/trestle/custom.js';
+	if ( is_readable( $upload_path . $custom_js_file ) )
+		wp_enqueue_script( 'trestle-custom-jquery', $upload_url . $custom_js_file, array( 'jquery' ), TRESTLE_THEME_VERSION, true );
+
+}
