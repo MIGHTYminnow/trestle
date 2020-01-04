6cbbb5a7f6af876aac3cbc98ac2b54770434e3f7
Aaron Barraza <aaron@mightyminnow.com>
2020-01-04 06:13:49 -0700

    TRESTLE 2 - jQuery: Add classes to different types of links.

--- includes/js/theme-jquery.js
+++ includes/js/theme-jquery.js
@@ -18,13 +18,13 @@ jQuery( document ).ready( function( $ ) {
 	// External Links.
 	var h = window.location.host.toLowerCase();
 	$( '[href^="http"]' ).not( '[href*="' + h + '"]' ).addClass( 'external-link' ).attr( "target", "_blank" );
-/*
+
 	// Add classes to different types of links.
 	$( 'a[href^="mailto:"]' ).addClass( 'email-link' );
 	$( 'a[href$=".pdf"]' ).attr({ "target":"_blank" }).addClass( 'pdf-link' );
 	$( 'a[href$=".doc"]' ).attr({ "target":"_blank" }).addClass( 'doc-link' );
 	$( 'a' ).has( 'img' ).addClass( 'image-link' );
-
+/*
 	// Add classes to parts of lists.
 	$( 'li:last-child' ).addClass( 'last' );
 	$( 'li:first-child' ).addClass( 'first' );
