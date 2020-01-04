ba04d19b5bc88309180a7318c838a09b0c7fd7fc
Aaron Barraza <aaron@mightyminnow.com>
2020-01-04 00:46:54 -0700

    TRESTLE 2: Unset Genesis default featured image fallback of 'first-attached'.

--- includes/functions/theme-functions.php
+++ includes/functions/theme-functions.php
@@ -416,3 +416,23 @@ function trestle_update_revisions_number( $num ) {
 
 	return $num;
 }
+
+add_filter( 'genesis_get_image_default_args', 'trestle_featured_image_fallback' );
+/**
+ * Unset Genesis default featured image fallback of 'first-attached'.
+ *
+ * This function prevents Genesis' default behavior of displaying
+ * the 'first-attached' image as a post's featured image (in archive)
+ * views, even when the post has no current featured image.
+ *
+ * @since 1.0.0
+ *
+ * @param array $args Default Genesis image args.
+ * @return array Updated image args.
+ */
+function trestle_featured_image_fallback( $args ) {
+
+	$args['fallback'] = false;
+
+	return $args;
+}
