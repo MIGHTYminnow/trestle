bf5004e3e4f35b5b1a9b7696dc2544adc81d5ddc
Aaron Barraza <aaron@mightyminnow.com>
2020-01-21 07:55:38 -0700

    Accessible Mega Menu: Move to Trestle Settings on Customizer and improve field description.

--- includes/class-accessible-mega-menu.php
+++ includes/class-accessible-mega-menu.php
@@ -2,7 +2,7 @@
 class Accessible_Mega_Menu {
 	public static function init() {
 		add_action( 'wp_enqueue_scripts', array( 'Accessible_Mega_Menu', 'enqueue_assets' ) );
-		add_filter( 'genesis_customizer_theme_settings_config', array( 'Accessible_Mega_Menu', 'set_customizer_settings' ) );
+		add_action( 'customize_register', array( 'Accessible_Mega_Menu', 'set_customizer_settings' ), 11 );
 		add_filter( 'genesis_attr_nav-primary', array( 'Accessible_Mega_Menu', 'set_nav_atts' ) );
 	}
 
@@ -16,27 +16,50 @@ class Accessible_Mega_Menu {
 		);
 	}
 
-	public static function set_customizer_settings( $config ) {
-		$config['genesis']['sections']['genesis_accessible_mega_menu'] = array(
-			'title' => __( 'Accessible Mega Menu', 'trestle' ),
-			'description' => __( 'Improves the accesibility of the main navigation menu by implementing keyboard navigation.', 'trestle' ),
-			'panel' => 'genesis',
-			'controls' => array(
-				'accessible_mega_menu' => array(
-					'label' => __( 'Activate Accessible Mega Menu', 'trestle' ),
-					'section' => 'genesis_accessible_mega_menu',
-					'type' => 'checkbox',
-					'settings' => [
-						'default' => false,
-					],
-				),
-			),
+	public static function set_customizer_settings( $wp_customize ) {
+		$wp_customize->add_setting(
+			'trestle-settings[mega_menu_title]',
+			array(
+				'default' => '',
+				'type'    => 'option',
+			)
 		);
-		return $config;
+
+		$wp_customize->add_control(
+			'trestle_mega_menu_title',
+			array(
+				'section' => 'trestle_settings_section',
+				'settings' => 'trestle-settings[mega_menu_title]',
+				'label' => __( 'Mega Menu', 'trestle' ),
+				'type' => 'hidden',
+			)
+		);
+
+		$wp_customize->add_setting(
+			'trestle-settings[mega_menu]',
+			array(
+				'default' => trestle_get_option( 'mega_menu' ),
+				'type' => 'option',
+				'capability' => 'edit_theme_options',
+			)
+		);
+		$wp_customize->add_control(
+			'trestle_mega_menu_control',
+			array(
+				'section' => 'trestle_settings_section',
+				'settings' => 'trestle-settings[mega_menu]',
+				'label' => __( 'Use Mega Menu', 'trestle' ),
+				'description' => __( 'Boost the accessibility of the already accessible main navigation menu by allowing you to navigate using the arrow keys (up/down/right/left) plus the default Tab navigation.<br><br>
+					Use the <b>left/right arrow keys</b> to jump to the next/previous submenu. <br>
+					Use the <b>up/down arrow keys</b> to go to the next/previous submenu item. <br><br>
+					<b>Please note that this option will disable clickability on the top level links of your menu if that top level link contains submenus. If you enable this, it is recommended to add the same link as the first item of the submenu.</b>', 'trestle' ),
+				'type' => 'checkbox',
+			)
+		);
 	}
 
 	public static function set_nav_atts( $attributes ) {
-		if ( genesis_get_option( 'accessible_mega_menu' ) ) {
+		if ( trestle_get_option( 'mega_menu' ) ) {
 			$attributes['class'] .= ' accessible-mega-menu';	
 		}
 		return $attributes;
